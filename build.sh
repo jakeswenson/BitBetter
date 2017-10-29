5c58b0d8fdb077be5c8ca155e26f81fc89bb8917
Jake Swenson <jake@jakeswenson.com>
2017-10-29 09:57:49 -0700

    adding license gen

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir ./src/bitbetter/.keys
+
+cp .keys/cert.cert ./src/bitbetter/.keys
+
+cd ./src/bitbetter
+
 dotnet restore
 dotnet publish
+
 docker build . -t bitbetter/api # --squash
+
