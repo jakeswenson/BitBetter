6d7675dcf84653b09168c186f80d01829d4c7fae
Jeff Alyanak <jeff@alyanak.ca>
2019-05-21 10:13:34 -0400

    Added subj to allow for non-interactive use.

--- .keys/generate-keys.sh
+++ .keys/generate-keys.sh
@@ -10,6 +10,6 @@ command -v openssl >/dev/null 2>&1 || { echo >&2 "openssl required but not found
 [ ! -e cert.pfx ]  || rm cert.pfx
 
 # Generate new keys
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.cert -days 36500 -outform DER -passout pass:test
+openssl	req -x509 -newkey rsa:4096 -keyout key.pem -out cert.cert -days 36500 -subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'  -outform DER -passout pass:test
 openssl x509 -inform DER -in cert.cert -out cert.pem
 openssl pkcs12 -export -out cert.pfx -inkey key.pem -in cert.pem -passin pass:test -passout pass:test
