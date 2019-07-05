783bde09f1b1c8a9be0dcb09516c0964cb1c6ea8
Vinrobot <vinrobot97@gmail.com>
2019-07-05 21:37:21 +0200

    Use same Dockerfile for api and identity images

--- build.sh
+++ build.sh
@@ -5,16 +5,13 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # If there aren't any keys, generate them first.
 [ -e "$DIR/.keys/cert.cert" ] || "$DIR/.keys/generate-keys.sh"
 
-[ -e "$DIR/src/bitBetter/api/.keys" ]  || mkdir "$DIR/src/bitBetter/api/.keys"
-[ -e "$DIR/src/bitBetter/identity/.keys" ]  || mkdir "$DIR/src/bitBetter/identity/.keys"
+[ -e "$DIR/src/bitBetter/.keys" ] || mkdir "$DIR/src/bitBetter/.keys"
 
-cp "$DIR/.keys/cert.cert" "$DIR/src/bitBetter/api/.keys"
-cp "$DIR/.keys/cert.cert" "$DIR/src/bitBetter/identity/.keys"
+cp "$DIR/.keys/cert.cert" "$DIR/src/bitBetter/.keys"
 
 docker run -v "$DIR/src/bitBetter:/bitBetter" -w=/bitBetter mcr.microsoft.com/dotnet/core/sdk:2.1 sh build.sh
 
-cp -r "$DIR/src/bitBetter/bin" "$DIR/src/bitBetter/api/"
-cp -r "$DIR/src/bitBetter/bin" "$DIR/src/bitBetter/identity/"
+docker build --build-arg BITWARDEN_TAG=bitwarden/api -t bitbetter/api "$DIR/src/bitBetter" # --squash
+docker build --build-arg BITWARDEN_TAG=bitwarden/identity -t bitbetter/identity "$DIR/src/bitBetter" # --squash
 
-docker build -t bitbetter/api "$DIR/src/bitBetter/api" # --squash
-docker build -t bitbetter/identity "$DIR/src/bitBetter/identity" # --squash
+
