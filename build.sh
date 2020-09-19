91d015572553435e8629cb8ba6edea35be1f67b8
h44z <christoph.h@sprinternet.at>
2020-09-20 00:36:15 +0200

    Fix build.sh and update script (#75) (#77)
    * possible fix for #75
    
    * Remove outdated bitbetter images as suggested by Ayitaka
    
    * Fix exit status if no old images have been removed

--- build.sh
+++ build.sh
@@ -15,11 +15,14 @@ cp "$DIR/.keys/cert.cert" "$DIR/src/bitBetter/.keys"
 
 docker run --rm -v "$DIR/src/bitBetter:/bitBetter" -w=/bitBetter mcr.microsoft.com/dotnet/core/sdk:3.1 sh build.sh
 
-docker build --no-cache --build-arg BITWARDEN_TAG=bitwarden/api:$BW_VERSION -t bitbetter/api "$DIR/src/bitBetter" # --squash
-docker build --no-cache --build-arg BITWARDEN_TAG=bitwarden/identity:$BW_VERSION -t bitbetter/identity "$DIR/src/bitBetter" # --squash
+docker build --no-cache --build-arg BITWARDEN_TAG=bitwarden/api:$BW_VERSION --label com.bitwarden.product="bitbetter" -t bitbetter/api "$DIR/src/bitBetter" # --squash
+docker build --no-cache --build-arg BITWARDEN_TAG=bitwarden/identity:$BW_VERSION --label com.bitwarden.product="bitbetter" -t bitbetter/identity "$DIR/src/bitBetter" # --squash
 
 docker tag bitbetter/api bitbetter/api:latest
 docker tag bitbetter/identity bitbetter/identity:latest
 docker tag bitbetter/api bitbetter/api:$BW_VERSION
 docker tag bitbetter/identity bitbetter/identity:$BW_VERSION
 
+# Remove old instances of the image after a successful build.
+ids=$( docker images bitbetter/* | grep -E -v -- "CREATED|latest|${BW_VERSION}" | awk '{ print $3 }' )
+[ -n "$ids" ] && docker rmi $ids || true
