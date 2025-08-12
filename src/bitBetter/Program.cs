f360f54e4655fe331af9aee06ab2e6e31139383f
Michiel Hazelhof <michiel@hazelhof.nl>
2025-08-12 12:10:31 +0200

    Update class path

--- src/bitBetter/Program.cs
+++ src/bitBetter/Program.cs
@@ -41,7 +41,7 @@ internal class Program
 
             Console.WriteLine($"New Cert Thumbprint: {certificate.Thumbprint}");
 
-            IEnumerable<TypeDef> services = moduleDefMd.Types.Where(t => t.Namespace == "Bit.Core.Services");
+            IEnumerable<TypeDef> services = moduleDefMd.Types.Where(t => t.Namespace == "Bit.Core.Billing.Services");
             TypeDef type = services.First(t => t.Name == "LicensingService");
             MethodDef constructor = type.FindConstructors().First();
             
