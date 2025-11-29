14d7d29845801c9c0ba7748db2afbe20bd93ab71
Jackson K <j6js@outlook.com>
2025-11-29 16:14:38 +1300

    Rename Containers from bitwarden/self-host:beta to bitwarden/lite:beta

--- build.ps1
+++ build.ps1
@@ -53,11 +53,11 @@ foreach ($instance in $oldinstances) {
 
 # update bitwarden itself
 if ($args[0] -eq 'update') {
-	docker pull ghcr.io/bitwarden/self-host:beta
+	docker pull ghcr.io/bitwarden/lite:beta
 } else {
 	$confirmation = Read-Host "Update (or get) bitwarden source container (y/n)"
 	if ($confirmation -eq 'y') {
-		docker pull ghcr.io/bitwarden/self-host:beta
+		docker pull ghcr.io/bitwarden/lite:beta
 	}
 }
 
@@ -80,7 +80,7 @@ foreach ($instance in $oldinstances) {
 }
 
 # start a new bitwarden instance so we can patch it
-$patchinstance = docker run -d --name bitwarden-extract ghcr.io/bitwarden/self-host:beta
+$patchinstance = docker run -d --name bitwarden-extract ghcr.io/bitwarden/lite:beta
 
 # create our temporary directory
 New-item -ItemType Directory -Path $tempdirectory
--- build.sh
+++ build.sh
@@ -54,11 +54,11 @@ done
 
 # update bitwarden itself
 if [ "$1" = "update" ]; then
-	docker pull ghcr.io/bitwarden/self-host:beta
+	docker pull ghcr.io/bitwarden/lite:beta
 else
 	read -p "Update (or get) bitwarden source container (y/n): "
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		docker pull ghcr.io/bitwarden/self-host:beta
+		docker pull ghcr.io/bitwarden/lite:beta
 	fi
 fi
 
@@ -81,7 +81,7 @@ for INSTANCE in ${OLDINSTANCES[@]}; do
 done
 
 # start a new bitwarden instance so we can patch it
-PATCHINSTANCE=$(docker run -d --name bitwarden-extract ghcr.io/bitwarden/self-host:beta)
+PATCHINSTANCE=$(docker run -d --name bitwarden-extract ghcr.io/bitwarden/lite:beta)
 
 # create our temporary directory
 mkdir $TEMPDIRECTORY
