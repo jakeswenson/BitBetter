0462ed9ad244d438599635e92eb42ef9dc2861d7
huntb4646 <94577767+huntb4646@users.noreply.github.com>
2026-06-01 20:21:49 -0400

    Update to .NET 10 in build.sh
    Copy .NET 10 runtime from aspnet10.0-alpine3.23 to the bitwarden-lite container

--- build.sh
+++ build.sh
@@ -108,7 +108,9 @@ docker run -v "$TEMPDIRECTORY:/app/mount" --rm bitbetter/bitbetter
 if [ -f "$PWD/Dockerfile-bitwarden-patch" ]; then
 	rm -f "$PWD/Dockerfile-bitwarden-patch"
 fi
+echo "FROM mcr.microsoft.com/dotnet/aspnet:10.0-alpine3.23" >> "$PWD/Dockerfile-bitwarden-patch"
 echo "FROM ghcr.io/bitwarden/lite:latest" >> "$PWD/Dockerfile-bitwarden-patch"
+echo "COPY --from=0 /usr/share/dotnet /usr/share/dotnet" >> "$PWD/Dockerfile-bitwarden-patch"
 for COMPONENT in ${COMPONENTS[@]}; do
 	echo "" >> "$PWD/Dockerfile-bitwarden-patch"
 	echo "RUN rm -f /app/$COMPONENT/$COMPONENT" >> "$PWD/Dockerfile-bitwarden-patch"
