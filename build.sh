e1604a0b78d1b9b85f52bd04773d118dadadec5c
vdmchkv <106989523+vdmchkv@users.noreply.github.com>
2026-06-15 22:46:31 +0200

    Add prerequisite checks and switch JSON parsing to jq
    The URL with the version of self-hosted Bitwarden is not working anymore. Now we're querying the official GitHub repository of Bitwarden and using jq to find the current released version. For simplification, we've moved to jq instead of sed/awk combinations and also added prerequisite checks for docker, openssl, and jq to avoid build failures.

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 set -e
 DIR=`dirname "$0"`
 DIR=`exec 2>/dev/null;(cd -- "$DIR") && cd -- "$DIR"|| cd "$DIR"; unset PWD; /usr/bin/pwd || /bin/pwd || pwd`
-BW_VERSION=$(curl -sL https://raw.githubusercontent.com/bitwarden/self-host/refs/heads/main/version.json | grep '^ *"'coreVersion'":' | awk -F\: '{ print $2 }' | sed -e 's/,$//' -e 's/^"//' -e 's/"$//')
+
+# Check prerequisite libraries and executables
+for cmd in docker curl openssl jq openssl; do
+    command -v "$cmd" >/dev/null 2>&1 || { echo "Error: '$cmd' is required but not installed." >&2; exit 1; }
+done
+BW_VERSION=$(curl -sL https://raw.githubusercontent.com/bitwarden/self-host/refs/heads/main/version.json | jq -r '.versions.coreVersion')
 
 echo "Building BitBetter for BitWarden version $BW_VERSION"
 
